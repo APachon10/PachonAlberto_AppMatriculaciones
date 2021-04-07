268198abb42de494b8f28d40469e3e7ee9dc7e9f
APachon10 <Pachondestroy95@gmail.com>
2021-04-07 19:48:53 +0200

    Cambios pequeños en la pagina de Login

--- Principal.js
+++ Principal.js
@@ -1,3 +1,25 @@
+//Importes 
 const express = require('express')
+const ejs= require('ejs')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken');
+
+//Variables
 const app = express()
 const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:false}))
+
+//Metodo Get que carga la pagina de login
+app.get('/', (req, res) => {
+   res.render('PantallaLogin.ejs')
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+
+
